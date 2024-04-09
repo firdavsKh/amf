f050035eb71b1b75f1c9eb7f04cb1fddaef03579
unknown <3djuverko@gmail.com>
2024-04-09 16:36:20 +0500

    test23

---
+++ possql/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tbl_fileupload (
+  id SERIAL PRIMARY KEY,
+  UID INT UNIQUE,
+  Message_ID VARCHAR(255) UNIQUE,
+  email_date DATE,
+  uploaded_date DATE,
+  email_from VARCHAR(255),
+  status INT,
+  chanel INT,
+  file_name VARCHAR(100),
+  file_dir VARCHAR(255),
+  file_hash VARCHAR(255)
+);
